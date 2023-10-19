57a0e735bb2de2280f5af3dadbbef7c86cffbe9a
Emily Law <emilyelhy@yahoo.com>
2023-10-20 00:32:59 +0800

    Testing router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Route, Navigate, Routes, BrowserRouter } from 'react-router-dom';
+
 import './App.css';
+
+import Login from './Component/Login';
 import Location from './Component/Location';
 
 function App() {
     return (
         <div className="App">
-            <Location/>
+            <BrowserRouter>
+				<Routes>
+					<Route exact path="/" element={<Navigate to="/login" />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/location" element={<Location />} />
+				</Routes>
+			</BrowserRouter>
         </div>
     );
 }
